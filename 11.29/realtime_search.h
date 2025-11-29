f3ce001ae1659e0924e4182055cc4e6f0086ea12
Chano363 <3334309819@qq.com>
2025-11-30 00:35:18 +0800

    11.29

--- 11.29/realtime_search.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "user.h"
-
-// 只声明必要的函数
-void realTimeAStarSearch(void);
-
-// 暂时注释掉可能缺失的函数
-// void executeOptimizedStep(MAZECOOR* path, int pathLength, int startIndex);
-// int calculateUnexploredPriority(int x, int y);
