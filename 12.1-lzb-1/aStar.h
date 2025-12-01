ba8f8b4efa6bf02ce8e6eea1fad6492be97aafbb
automation2006 <Lizhengbbao@hotmail.com>
2025-12-01 19:00:14 +0800

    12.1-1
    对A*冲刺部分进一步优化，使冲刺过程中拐点减少，有效减少了时间

--- 12.1-lzb-1/aStar.h
+++ 12.1-lzb-1/aStar.h
@@ -1,6 +1,5 @@
 #pragma once
-
-#include"user.h"
+#include "user.h"
 
 #define MAX_PATH_LENGTH (MAZETYPE * MAZETYPE)
 
@@ -12,7 +11,13 @@ struct AStarNode {
 };
 typedef struct AStarNode AStarNode;
 
+// 函数声明
 int aStarSearch(int startX, int startY, int goalX, int goalY, MAZECOOR* path, int* pathLength);
+void aStarMethod(void);
 int heuristic(int x1, int y1, int x2, int y2);
 void getNeighbors(int x, int y, MAZECOOR neighbors[], int* count);
-void aStarMethod(void);
+void smoothPath(MAZECOOR* path, int* pathLength);
+
+// 外部函数声明（在user.c中定义）
+extern MAZECOOR findNearestUnexplored(void);
+extern void floodFillMethod(void);
