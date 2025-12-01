180f08f788dcb3da429fd1a73c43d89ffc3fd21d
to-Seele <2307868572@qq.com>
2025-12-02 02:51:04 +0800

    12.1
    基础动作参数初步修改，直行加速度提高最大，回转左转和右转加速度微调；新建分基础动作文件夹和总基础动作文件夹，总基础动作文件夹内程序用于提交ds,还没和分动作程序的参数同步

---
+++ 基础动作/gostraight/user.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include"simulation.h"
+#pragma comment(lib,"simulation.lib")
+/**/
+
+int userMain(void)
+{
+	int switchmodel = 3;	//0-右转	1-左转	2-回转	3-直行
+	if (switchmodel == 3)
+	{
+		double dist = 0;
+		double speedLMax = 0;
+		double speedRMax = 0;
+		double accelerate = 9;
+
+		readMessage();
+		Get_RandomDcMotor();
+
+		SetPID_Right(11, 0.4, 0.8);
+		SetPID_Left(14.176, 0.4, 0.8);
+
+		while (dist <= 4)
+		{
+			SetSpeed_Right(Get_speedRight() + accelerate);
+			MotorRight_Update();
+			DcMotor_Right();
+
+			SetSpeed_Left(Get_speedLeft() + accelerate);
+			MotorLeft_Update();
+			DcMotor_Left();
+			Save_Speed();
+			speedLMax = Get_speedLeft();
+			speedRMax = Get_speedRight();
+			dist = GetDistCent();
+		}
+
+		while (dist <= 12)
+		{
+			SetSpeed_Right(speedRMax);
+			MotorRight_Update();
+			DcMotor_Right();
+
+			SetSpeed_Left(speedLMax);
+			MotorLeft_Update();
+			DcMotor_Left();
+			Save_Speed();
+			dist = GetDistCent();
+		}
+
+		while (dist <= 16)
+		{
+			if (abs(Get_speedRight()) < 0.001 || abs(Get_speedLeft()) < 0.001)
+			{
+				break;
+			}
+			SetSpeed_Right(Get_speedRight() - accelerate);
+			MotorRight_Update();
+			DcMotor_Right();
+
+			SetSpeed_Left(Get_speedLeft() - accelerate);
+			MotorLeft_Update();
+			DcMotor_Left();
+			Save_Speed();
+			dist = GetDistCent();
+			if (Get_speedRight() < 0 || Get_speedLeft() < 0)
+			{
+				SetSpeed_Right(Get_speedRight() + accelerate);
+				MotorRight_Update();
+				DcMotor_Right();
+
+				SetSpeed_Left(Get_speedLeft() + accelerate);
+				MotorLeft_Update();
+				DcMotor_Left();
+				Save_Speed();
+				dist = GetDistCent();
+			}
+		}
+		Trans_Speed(speedLMax, speedRMax);	//左轮&右轮期望顶棚速度
+	}
+}
